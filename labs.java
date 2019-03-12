049ca6a0b2accddd989daf88634d83f82a7fe42c
James <42484571+smitteh1@users.noreply.github.com>
2019-03-12 14:17:40 -0400

    Added Factorials

--- labs.java
+++ labs.java
@@ -12,6 +12,10 @@ public class labs {
 	// Exp used in Power Method
   	int powerExp = 6;
 	
+	// Factorial to be calculated
+	int factorial = 7;
+
+	
 	public static void main(String[] args) {
 		// Use constructor
 		labs t = new labs();
@@ -22,6 +26,7 @@ public class labs {
 		System.out.println("Exp ("+ powerBase + "^" + powerExp + ") : " + power(powerBase, powerExp));
 		System.out.println("digitCount (" + digitC + ") : " + digitCount(digitC));
 		System.out.println("sumDigits (" + sumDig + ") : " + sumDigits(sumDig));
+		System.out.println("!Factorial (!" + factorial + ") : " + f(factorial));
 		System.out.print("Fib Numbers (" + numbersOutput + ") : ");
 		for (int x = 0; x < numbersOutput; x++) {
 			System.out.print(fib(x) + ", ");
@@ -59,4 +64,11 @@ public class labs {
 			if (exo == 2) return base * base;
 			else
 				return base * power(base, exo-1);
+	}
+	
+	// Recursive factorial method
+	public int f(int n) {
+		if (n == 1) return 1;
+			else
+				return n * f(n-1);
 	}
